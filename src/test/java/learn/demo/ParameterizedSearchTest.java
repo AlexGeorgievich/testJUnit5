197cac2dcc684339a74d2e49136c1ecb40b4ad08
AlexGeorgievich <alexhome1@yandex.ru>
2022-03-02 16:39:24 +0300

    ошибки в прохождении параметризованного теста при поиске  "Selenide,: Concise UI tests in Java", не проходит               "JUnit5,The IntelliJ IDEA Blog"     проходит               сообщение об ошибки локатора,               хотф локатор тот же

--- src/test/java/learn/demo/ParameterizedSearchTest.java
+++ src/test/java/learn/demo/ParameterizedSearchTest.java
@@ -2,34 +2,67 @@ package learn.demo;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
 
 public class ParameterizedSearchTest {
-    @BeforeEach
-    Configuration.browser = "firefox";
-    Configuration.browserSize = "1920x1080";
 
-    void precondition(){
+    @BeforeEach
+    void precondition() {
+//        Configuration.browser ="firefox";
+//        Configuration.browserSize ="1920x1080";
         open("https://ya.ru");
     }
+
     @AfterEach
-    void closeBrowser(){
+    void closeBrowser() {
         Selenide.closeWebDriver();
     }
+/*
     @Test
-    @DisplayName("Заголовок отображения поисковых результатов вяндекс для запроса Selenide")
-    void searchTest () {
+    @DisplayName("Заголовок отображения поисковых результатов в яндекс для запроса Selenide")
+    void searchTest() {
         // Шаги
         Selenide.$("#text").setValue("Selenide");
-//        Selenide.$("button.button[type='submit'] ").click();
         Selenide.$("button[type='submit']").click();
-        Selenide.$$( " ").find(text("Selenide")).shouldBe(visible);
+        Selenide.$$("li.serp-item").find(text("Selenide")).shouldBe(visible);
     }
+
+    @Test
+    @DisplayName("Заголовок отображения поисковых результатов в яндекс для запроса JUnit5")
+    void searchJUnit5Test() {
+        // Шаги
+        Selenide.$("#text").setValue("JUnit5");
+        Selenide.$("button[type='submit']").click();
+        Selenide.$$("li.serp-item").find(text("JUnit5")).shouldBe(visible);
+    }
+
+    @ValueSource(strings = {"Selenide", "JUnit5"})
+    @ParameterizedTest(name = " Проверка наличия в поиске запроса в Ян \"{0}\"")
+    void commonSearchTest(String testData) {
+        // Шаги
+        Selenide.$("#text").setValue(testData);
+        Selenide.$("button[type='submit']").click();
+        Selenide.$$("li.serp-item").find(text(testData)).shouldBe(visible);
+    }
+
+*/
+    @CsvSource({
+            "Selenide,: Concise UI tests in Java",
+            "JUnit5,The IntelliJ IDEA Blog"
+        })
+    @ParameterizedTest(name = " Проверка наличия в поиске запроса в Ян \"{0}\"")
+    void commonSearchTest(String testData, String expectedText) {
+        // Шаги
+        Selenide.$("#text").setValue(testData);
+        Selenide.$("button[type='submit']").click();
+        Selenide.$$("li.serp-item").find(text(expectedText)).shouldBe(visible);
+    }
+
 }
